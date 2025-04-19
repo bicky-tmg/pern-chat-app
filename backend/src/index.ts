1a513d80010448e36b0ccb99eb53799c947eabcd
Bicky Tamang <bickytamang101@gmail.com>
2025-04-19 20:44:21 +0545

    feat: app init and models for user conversation and message created

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
