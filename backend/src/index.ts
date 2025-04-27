12b0bd4cfb835baf5cfb1a79ab3253bb1bd81288
Bicky Tamang <bickytamang101@gmail.com>
2025-04-27 11:07:53 +0545

    feat: signup, login and logout api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // for parsing application/json
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
